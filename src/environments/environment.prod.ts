5e9928dde28beebea541c9d5d9e1111710a1a535
mr-mateus <mateus395@gmail.com>
2020-03-23 09:18:14 -0300

    ajuste na api para ser https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  OMDbAPI: "http://www.omdbapi.com/?apikey=792817af"
+  OMDbAPI: "https://www.omdbapi.com/?apikey=792817af"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  OMDbAPI: "http://www.omdbapi.com/?apikey=792817af"
+  OMDbAPI: "https://www.omdbapi.com/?apikey=792817af"
 };
 
 /*
